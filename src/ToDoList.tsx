526b7c868f796fa8a736d8b7a69ffc705fc76ad9
sangminlee98 <tkdals0975@naver.com>
2022-04-11 00:55:37 +0900

    install react-hook-form library

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 
 const ToDoList = () => {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     setToDo(e.currentTarget.value);
+    setToDoError("");
   }
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if(toDo.length < 10) {
+      return setToDoError('To do should be longer')
+    }
     console.log(toDo);
+    console.log('submit');
     setToDo("");
   }
   return (
@@ -15,6 +21,7 @@ const ToDoList = () => {
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder='Write a to do' />
         <button>Add</button>
+        {toDoError !== '' ? toDoError : null}
       </form>
     </div>
   );
