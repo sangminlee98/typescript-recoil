d159bed1d0551cad05d3f7d28583cce837e8e5ac
sangminlee98 <tkdals0975@naver.com>
2022-04-10 21:53:36 +0900

    setting project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {RecoilRoot} from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { darkTheme } from './theme';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElem = document.getElementById('root')!;
+const root = createRoot(rootElem);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App/>
+      </ThemeProvider>
+    </RecoilRoot>
+  </StrictMode>
+)
