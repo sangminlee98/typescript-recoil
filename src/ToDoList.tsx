fc0c4344c620d3c157b43284e31771bfcb76c34a
sangminlee98 <tkdals0975@naver.com>
2022-04-11 01:08:13 +0900

    [MODIFY] modify form by using react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+// const ToDoList = () => {
+//   const [toDo, setToDo] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+//     setToDo(e.currentTarget.value);
+//     setToDoError("");
+//   }
+//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if(toDo.length < 10) {
+//       return setToDoError('To do should be longer')
+//     }
+//     console.log(toDo);
+//     console.log('submit');
+//     setToDo("");
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={toDo} placeholder='Write a to do' />
+//         <button>Add</button>
+//         {toDoError !== '' ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// };
+// export default ToDoList;
 
 const ToDoList = () => {
-  const [toDo, setToDo] = useState("");
-  const [toDoError, setToDoError] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setToDo(e.currentTarget.value);
-    setToDoError("");
-  }
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if(toDo.length < 10) {
-      return setToDoError('To do should be longer')
-    }
-    console.log(toDo);
-    console.log('submit');
-    setToDo("");
-  }
+  const {register, watch} = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder='Write a to do' />
+      <form>
+        <input {...register('Email')} placeholder='Email' />
+        <input {...register('First Name')} placeholder='First Name' />
+        <input {...register('Last Name')} placeholder='Last Name' />
+        <input {...register('Username')} placeholder='Username' />
+        <input {...register('Password')} placeholder='Password' />
+        <input {...register('Password1')} placeholder='Password1' />
         <button>Add</button>
-        {toDoError !== '' ? toDoError : null}
       </form>
     </div>
   );
