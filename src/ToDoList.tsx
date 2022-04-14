9fa25d687add918fb6582ded22792a71e01470f2
sangminlee98 <tkdals0975@naver.com>
2022-04-15 01:44:12 +0900

    [ADD] ToDoList component

---
+++ src/ToDoList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const ToDoList = () => {
+  return (
+    <div>
+      <form>
+        <input placeholder='Write a to do' />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+};
+export default ToDoList;
