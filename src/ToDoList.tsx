434c4b251b5de114669347bf87684f3b2835f1ce
sangminlee98 <tkdals0975@naver.com>
2022-04-15 20:26:39 +0900

    [IMPLEMENT] todo form using recoil

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { atom, useRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+const ErrorMessage = styled.span`
+  margin-left: 1em;
+  font-size: 0.8rem;
+  color: red;
+`;
+
+const toDoState = atom<IToDo[]>({
+  key: 'toDo',
+  default: [],
+})
 
 interface IToDo {
+  text: string;
+  id: number;
+  category: 'TO_DO' | 'DOING' | 'DONE';
+}
+interface IForm {
   toDo: string;
 }
 const ToDoList = () => {
-  const {
-    register, handleSubmit, formState:{errors}, setValue
-  } = useForm<IToDo>();
-  const onSubmit = (data: IToDo) => {
-    console.log('add to do', data.toDo);
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const {register, handleSubmit, formState:{errors}, setValue} = useForm<IForm>();
+  const handleValid = ({toDo}: IForm) => {
+    setToDos(prev => [...prev, {text: toDo,id: Date.now(), category: 'TO_DO'}]);
     setValue('toDo', '');
   }
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit, () => console.log('error'))}>
-        <input {...register('toDo', {required: 'please write To a Do'})} placeholder='Write a to do' />
-        {errors.toDo?.message}
+      <h1>To Dos</h1>
+      <form onSubmit={handleSubmit(handleValid, () => console.log('error'))} autoComplete='off'>
+        <input {...register('toDo', {required: 'please write a To Do'})} placeholder='Write a to do' />
         <button>Add</button>
+        <ErrorMessage>{errors.toDo?.message}</ErrorMessage>
       </form>
+      <ul>
+        {toDos.map((todo) => (<li key={todo.id}>{todo.text}</li>))}
+      </ul>
     </div>
   );
 };
