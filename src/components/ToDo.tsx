7a809a797d0606bbb0be5f3bb4c78ac7e59e5222
sangminlee98 <tkdals0975@naver.com>
2022-04-17 01:12:15 +0900

    [IMPLEMENT] delete button

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -23,11 +23,28 @@ const Button = styled.button`
   }
 `;
 
+const DeleteButton = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-size: 0.8rem;
+  color: red;
+  background-color: transparent;
+  transition: all .2s ease-in;
+  &:hover {
+    opacity: 0.8;
+    transform: scale(1.1);
+  }
+`;
+
 const ToDo = ({text, category, id}: IToDo) => {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const name = e.currentTarget.name as any;
     setToDos(prev => prev.map(toDo => toDo.id === id ? {...toDo, category: name} : toDo));
+  }
+  const onDelete = () => {
+    setToDos(prev => prev.filter(toDo => toDo.id !== id));
   }
   return (
     <li>
@@ -35,6 +52,7 @@ const ToDo = ({text, category, id}: IToDo) => {
       {category !== Categories.TO_DO && <Button name={Categories.TO_DO} onClick={onClick}>To Do</Button>}
       {category !== Categories.DOING && <Button name={Categories.DOING} onClick={onClick}>Doing</Button>}
       {category !== Categories.DONE && <Button name={Categories.DONE} onClick={onClick}>Done</Button>}
+      <DeleteButton onClick={onDelete}>Delete</DeleteButton>
     </li>
   );
 };
